08abeb065bfdc1fcb14a3375dc8ce6a67f0a6687
Marcos <35502465+marcosmcampos@users.noreply.github.com>
2020-05-24 15:40:35 +0100

    Marcos 24 Maio - pagina login feita

--- src/com/istec/main/Main.java
+++ src/com/istec/main/Main.java
@@ -1,5 +1,13 @@
 package com.istec.main;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+import java.awt.FlowLayout;
+
 import com.istec.objectos.Admin;
 import com.istec.objectos.Engine;
 import com.istec.objectos.Store;
@@ -12,6 +20,29 @@ public class Main {
 	}
 
 	public static void main(String[] args) throws Exception {
+		//marcos start 24 Maio
+		
+		//este fdp fez isto tudo à mão só para mostrar como criar janelas por código e depois cagou nisto
+//		JFrame frame = new JFrame();
+//		JPanel panel = new JPanel();
+//		panel.setLayout(new FlowLayout());
+//		
+//		JLabel label = new JLabel("o eclipse não vale um peido");
+//		JButton btn = new JButton("abrir o visual studio");
+//		
+//		panel.add(label);
+//		panel.add(btn);
+//		
+//		frame.add(panel);
+//		frame.setBounds(100,100,400,400);
+//		
+//		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//		
+//		frame.setVisible(true);
+//		
+		//marcos end 24 Maio
+		
+		
 		System.out.println("--- start ---");
 		//Engine.getInstance();
 		
