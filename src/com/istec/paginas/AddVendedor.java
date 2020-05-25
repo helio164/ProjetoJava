b0c48db2abc7ca3a24fa2fbf96b3fb6600fe971e
Aneliko <ideiasconstrutivas@gmail.com>
2020-05-25 23:43:16 +0100

    update 23h43

--- src/com/istec/paginas/AddVendedor.java
+++ src/com/istec/paginas/AddVendedor.java
@@ -19,13 +19,38 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
 
-public class AddVendedor extends JFrame{
+public class AddVendedor extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AddVendedor frame = new AddVendedor();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
 	public AddVendedor() {
-	
-	
-	
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
 	
 	this.setVisible(true);
+	}
 }
-	
-}
+
